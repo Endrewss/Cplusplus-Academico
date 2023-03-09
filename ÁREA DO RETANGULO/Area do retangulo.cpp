349396a6d9869c3f01e2f10071016c6ff700b152
Endrews Azevedo <98989141+Endrewss@users.noreply.github.com>
2023-03-09 08:59:26 -0300

    Add Files Exercise Área do Retângulo

---
+++ ÁREA DO RETANGULO/Area do retangulo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int n1;
+	cout << "Informe o 1 lado do retangulo: ";
+	cin >> n1;
+	
+	int n2;
+	cout << "Informe o 1 lado do retangulo: ";
+	cin >> n2;
+	
+	float area = n1 * n2;
+	cout << "Area do retangulo = " << area;
+	
+	return 0;
+}
